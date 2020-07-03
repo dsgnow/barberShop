d19c3549e6e9a7953e9cfe5a14109f59e7ccd34b
workStation <ptr.stachowicz@gmail.com>
2020-07-03 15:51:03 +0200

    animation about fix

--- js/main.js
+++ js/main.js
@@ -13,12 +13,20 @@ $(document).on('scroll', function () {
     const $aboutTitle = $('.columnTextsAbout_title');
     const $aboutText = $('.columnTextsAbout__descriptionText')
     const $aboutLogoElement = $('.columnTextsAbout__imgLogo')
+    const $aboutImageDawid = $('.columnImagesAbout__imgDawid')
+
+    const fadeIntexts = document.querySelectorAll('.columnImagesAbout__text');
 
 
     if (scrollValue > aboutBlockTextsHeight + aboutBlockTextsFromTop - windowHeight - 200) {
         $aboutTitle.addClass("columnTextsAbout_title--active");
         $aboutText.addClass("columnTextsAbout__descriptionText--active");
         $aboutLogoElement.addClass("columnTextsAbout__imgLogo--active");
+        $aboutImageDawid.addClass("columnImagesAbout__imgDawid--active");
+
+        fadeIntexts.forEach(function (fadeInText, index) {
+            fadeInText.classList.add("columnImagesAbout__text--fadeIn");
+        });
     }
 
     //clean
@@ -26,4 +34,22 @@ $(document).on('scroll', function () {
 
     }
 
+})
+
+
+// animation to section when click menu //
+$('nav a').on('click', function () {
+    const goToSection = "[data-sectionin=" + $(this).data('section') + "]";
+    $('body, html').animate({
+        scrollTop: $(goToSection).offset().top
+    }, 500)
+})
+
+$('.hambPopup a').on('click', function () {
+    hambPop.classList.remove('show');
+    hambButtonI.classList.toggle('fa-times');
+    const goToSection = "[data-section=" + $(this).attr('class') + "]";
+    $('body, html').animate({
+        scrollTop: $(goToSection).offset().top
+    }, 500)
 })
