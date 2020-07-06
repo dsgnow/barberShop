af1e6e365872757f9b7fdf115ae88b233cefa17e
Piotr Stachowicz <ptr.stachowicz@gmail.com>
2020-07-06 20:37:08 +0200

    add plan animation slider

--- js/main.js
+++ js/main.js
@@ -36,38 +36,66 @@ const $hambPop = $('.navBurger');
 const $hambButton = $('.navBurger__btn');
 const $hambSpan = $('.navBurger__span');
 
+const $navLinks = $('.nav__link');
+
 
 // appear elements when scroll //
 $(document).on('scroll', function () {
 
     const windowHeight = $(window).height();
     const scrollValue = $(window).scrollTop();
+    let orientation = window.screen.orientation;
 
     // change link border on scroll //
     if ((scrollValue < windowHeight)) {
         clearLinksBorder();
         linkStart.classList.add('nav__link--selected');
+        if (orientation.type == 'landscape-primary') {
+            $hambSpan.addClass('navBurger__span--white');
+            $hambSpan.removeClass('navBurger__span--black');
+            $navLinks.removeClass('nav__link--black');
+        } else if (orientation.type == 'portrait-primary') {
+            $hambSpan.addClass('navBurger__span--black');
+            $hambSpan.removeClass('navBurger__span--white');
+        }
 
     } else if ((scrollValue >= windowHeight) && (scrollValue < windowHeight * 2)) {
         clearLinksBorder();
         linkAbout.classList.add('nav__link--selected');
+        if (orientation.type == 'landscape-primary') {
+            $hambSpan.addClass('navBurger__span--white');
+            $hambSpan.removeClass('navBurger__span--black');
+            $navLinks.removeClass('nav__link--black');
+        } else if (orientation.type == 'portrait-primary') {
+            $hambSpan.addClass('navBurger__span--white');
+            $hambSpan.removeClass('navBurger__span--black');
+        }
 
     } else if ((scrollValue >= windowHeight) && (scrollValue < windowHeight * 3)) {
         clearLinksBorder();
         linkPlan.classList.add('nav__link--selected');
+        if (orientation.type == 'landscape-primary') {
+            $hambSpan.addClass('navBurger__span--black');
+            $hambSpan.removeClass('navBurger__span--white');
+            $navLinks.addClass('nav__link--black');
+        } else if (orientation.type == 'portrait-primary') {
+            $hambSpan.addClass('navBurger__span--black');
+            $hambSpan.removeClass('navBurger__span--white');
+        }
 
     } else if ((scrollValue >= windowHeight) && (scrollValue < windowHeight * 4)) {
         clearLinksBorder();
         linkContact.classList.add('nav__link--selected');
+        if (orientation == 'landscape-primary') {
+            $hambSpan.addClass('navBurger__span--white');
+            $hambSpan.removeClass('navBurger__span--black');
+        } else if (orientation.type == 'portrait-primary') {
+            $hambSpan.addClass('navBurger__span--white');
+            $hambSpan.removeClass('navBurger__span--black');
+        }
     }
 
-    // let orientation = window.screen.orientation;
-    /* change burger icon color on scroll */
-    if ((scrollValue > aboutHeight + aboutFromTop - windowHeight - 50)) {
-        $hambSpan.addClass('navBurger__span--white');
-    } else {
-        $hambSpan.removeClass('navBurger__span--white');
-    }
+
 
     if (scrollValue > aboutBlockTextsHeight + aboutBlockTextsFromTop - windowHeight - 200) {
         $aboutTitle.addClass("columnTextsAbout_title--active");
@@ -123,8 +151,15 @@ const leftSlide = document.querySelector('.offer__navigation--left');
 
 const offers = [...document.querySelectorAll('.offer__option')];
 
-const slideIt = function () {
-    let planIndex = '';
+const slideIt = function (side) {
+
+    rightSlide.style.pointerEvents = "none";
+    leftSlide.style.pointerEvents = "none";
+    setTimeout(function () {
+        rightSlide.style.pointerEvents = "auto";
+        leftSlide.style.pointerEvents = "auto";
+    }, 2000);
+
     offers.forEach(function (offer, index) {
         if (offer.classList.contains('offer__option--active')) {
             planIndex = index;
@@ -132,21 +167,42 @@ const slideIt = function () {
 
     });
 
-    if (planIndex !== offers.length - 1) {
-        offers[planIndex].classList.remove('offer__option--active');
-        offers[planIndex + 1].classList.add('offer__option--active');
-        planIndex++;
+    if (this.classList.contains('offer__navigation--right')) {
+
+        if (planIndex !== offers.length - 1) {
+            offers[planIndex].classList.remove('offer__option--removalLeft');
+            offers[planIndex].classList.remove('offer__option--active');
+            offers[planIndex].classList.remove('offer__option--showInRight');
+            offers[planIndex].classList.remove('offer__option--showInLeft');
+            offers[planIndex].classList.add('offer__option--removalRight');
+            offers[planIndex + 1].classList.add('offer__option--showInRight', 'offer__option--active');
+            planIndex++;
+        }
+    } else if (this.classList.contains('offer__navigation--left')) {
+
+        if (planIndex !== 0) {
+            offers[planIndex].classList.remove('offer__option--removalRight');
+            offers[planIndex].classList.remove('offer__option--active');
+            offers[planIndex].classList.remove('offer__option--showInLeft');
+            offers[planIndex].classList.remove('offer__option--showInRight');
+            offers[planIndex].classList.add('offer__option--removalLeft');
+            offers[planIndex - 1].classList.add('offer__option--showInLeft', 'offer__option--active');
+            planIndex--;
+        }
     }
 
+    console.log(offers.length);
     if (planIndex == 0) {
         leftSlide.classList.remove('offer__navigation--active');
-    } else if ((planIndex > 0) && (planIndex !== offers.length)) {
+        rightSlide.classList.add('offer__navigation--active');
+    } else if ((planIndex > 0) && (planIndex !== offers.length - 1)) {
         leftSlide.classList.add('offer__navigation--active');
-    } else if ((planIndex > 0) && (planIndex == offers.length)) {
+        rightSlide.classList.add('offer__navigation--active');
+    } else if ((planIndex > 0) && (planIndex == offers.length - 1)) {
         rightSlide.classList.remove('offer__navigation--active');
     }
-
-
 }
-
+const right = "right";
+const left = "left";
 rightSlide.addEventListener('click', slideIt);
+leftSlide.addEventListener('click', slideIt);
